f88bd947ba0b932c564021bbeaf8ebd762c49611
Enrique Ruvalcaba <jordison352@gmail.com>
2023-08-29 21:17:40 -0600

    feat: create main layout

---
+++ src/layout/MainLayout.tsx
@@ -0,0 +1,21 @@
+import { FC, PropsWithChildren } from 'react';
+import Header from '@components/Header';
+import Nav from '@common/Nav';
+
+const MainLayout: FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <>
+      <div className="min-h-full">
+        <Header />
+        <Nav />
+        <main>
+          <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+            {children}
+          </div>
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default MainLayout;
